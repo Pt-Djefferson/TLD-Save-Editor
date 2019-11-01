45bd0f2e70212bf9c515e7355ec1689426746265
Pt-Djefferson <alex.tesla@gmail.com>
2019-11-01 21:50:06 +0900

    teleport

--- The Long Dark Save Editor 2/Tabs/MapTab.xaml.cs
+++ The Long Dark Save Editor 2/Tabs/MapTab.xaml.cs
@@ -24,7 +24,8 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
         private ScaleTransform rbScaleTransform;
         private TranslateTransform rbTranslateTransform;
 
-        private RadioButton combo;
+        private RadioButton rbButton;
+        private Label rbLabel;
 
         public MapTab()
         {
@@ -42,7 +43,8 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
                         return;
                     }
                     region = MainWindow.Instance.CurrentSave.Boot.m_SceneName.Value;
-                    playerPosition = new Point(MainWindow.Instance.CurrentSave.Global.PlayerManager.m_SaveGamePosition[0], MainWindow.Instance.CurrentSave.Global.PlayerManager.m_SaveGamePosition[2]);
+                    //playerPosition = new Point(MainWindow.Instance.CurrentSave.Global.PlayerManager.m_SaveGamePosition[0], MainWindow.Instance.CurrentSave.Global.PlayerManager.m_SaveGamePosition[2]);
+                    playerPosition = new Point(-1197.492, 998.6943);
                     UpdateMap();
                     var saveGamePosition = MainWindow.Instance.CurrentSave.Global.PlayerManager.m_SaveGamePosition;
                     saveGamePosition.CollectionChanged += (sender2, e2) =>
@@ -71,6 +73,7 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
 
         private void UpdateMap()
         {
+            return;
             if (!IsLoaded)
                 return;
             if (region == null)
@@ -104,17 +107,18 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
             scale.ScaleX = Math.Max(Math.Min(wScale, hScale), 0.5);
             scale.ScaleY = Math.Max(Math.Min(wScale, hScale), 0.5);
 
+            scaleVB.ScaleX = scale.ScaleX;
+            scaleVB.ScaleY = scale.ScaleY;
+
+            //rbLayer.Width = mapImage.Width;
+            //rbLayer.Height = mapImage.Height;
+
             rbScaleTransform = new ScaleTransform();
             rbTranslateTransform = new TranslateTransform();
             TransformGroup rbTransformGroup = new TransformGroup();
             rbTransformGroup.Children.Add(rbScaleTransform);
             rbTransformGroup.Children.Add(rbTranslateTransform);
 
-            int zOrder = Canvas.GetZIndex(mapImage);
-/*            foreach (UIElement child in canvas.Children)
-            {
-                child.
-            }*/
             for (int i = 0; i < canvas.Children.Count; i++)
             {
                 if (canvas.Children[i] is RadioButton)
@@ -122,24 +126,27 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
                     canvas.Children.RemoveAt(i);
                 }
             }
-            combo = new RadioButton();
-            canvas.Children.Add(combo);
-            combo.Visibility = Visibility.Visible;
-            Canvas.SetZIndex(combo, (int)99);
-            combo.Content = "";
-            combo.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-            combo.RenderTransformOrigin = new Point(0.5, 0.5);
-            combo.RenderTransform = rbTransformGroup;
 
-            rbScaleTransform.ScaleX = scale.ScaleX;
-            rbScaleTransform.ScaleY = scale.ScaleY;
+            rbButton = new RadioButton();
+            canvas.Children.Add(rbButton);
+            //combo.Visibility = Visibility.Visible;
+            //Canvas.SetZIndex(combo, (int)99);
+            //combo.Content = "";
+            rbButton.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            rbButton.RenderTransformOrigin = new Point(0.5, 0.5);
+            rbButton.RenderTransform = rbTransformGroup;
+
             
-            //rbTranslateTransform.X = 30;
-            //rbTranslateTransform.Y = 30;
 
             SetPosition(0, 0);
-            Canvas.SetLeft(combo, ((-1197.492 * mapInfo.pixelsPerCoordinate + mapInfo.origo.X) * scale.ScaleX + Canvas.GetLeft(mapImage)) - combo.ActualWidth / 2);
-            Canvas.SetTop(combo, ((998.6943 * -mapInfo.pixelsPerCoordinate + mapInfo.origo.Y) * scale.ScaleY + Canvas.GetTop(mapImage)) - combo.ActualHeight / 2);
+            rbScaleTransform.ScaleX = scale.ScaleX;
+            rbScaleTransform.ScaleY = scale.ScaleY;
+            //rbTranslateTransform.X = - combo.ActualWidth;
+            //rbTranslateTransform.Y = - combo.ActualHeight;
+            //Canvas.SetLeft(rbButton, 10);
+            //Canvas.SetTop(rbButton, 10);
+            Canvas.SetLeft(rbButton, ((-1197.492 * mapInfo.pixelsPerCoordinate + mapInfo.origo.X) * scale.ScaleX + Canvas.GetLeft(mapImage)) - 18 / 2);
+            Canvas.SetTop(rbButton, ((998.6943 * -mapInfo.pixelsPerCoordinate + mapInfo.origo.Y) * scale.ScaleY + Canvas.GetTop(mapImage)) - 18 / 2);
         }
 
         private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -198,8 +205,14 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
             scale.ScaleX += zoom;
             scale.ScaleY += zoom;
 
-            scaleR.ScaleX += zoom;
-            scaleR.ScaleY += zoom;
+            //scaleR.ScaleX += zoom;
+            //scaleR.ScaleY += zoom;
+
+            //scaleRBLayer.ScaleX += zoom;
+            //scaleRBLayer.ScaleY += zoom;
+
+            scaleVB.ScaleX += zoom;
+            scaleVB.ScaleY += zoom;
 
             rbScaleTransform.ScaleX += zoom;
             rbScaleTransform.ScaleY += zoom;
@@ -211,18 +224,20 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
 
         private void SetPosition(double x, double y)
         {
+            rbTranslateTransform.X = (-1197.492 * mapInfo.pixelsPerCoordinate + mapInfo.origo.X) * (scale.ScaleX - 0.5) + x;
+            rbTranslateTransform.Y = (998.6943 * -mapInfo.pixelsPerCoordinate + mapInfo.origo.Y) * (scale.ScaleY - 0.5) + y;
+
             Canvas.SetLeft(mapImage, x);
             Canvas.SetTop(mapImage, y);
+            Canvas.SetLeft(rbLayer, x);
+            Canvas.SetTop(rbLayer, y);
 
             UpdatePlayerPosition(x, y);
 
-            //rbTranslateTransform.X = x;
-            //rbTranslateTransform.Y = y;
-
-            //var xRB = (playerPosition.X * mapInfo.pixelsPerCoordinate + mapInfo.origo.X) * scale.ScaleX + Canvas.GetLeft(mapImage);
-            //var yRB = (playerPosition.Y * -mapInfo.pixelsPerCoordinate + mapInfo.origo.Y) * scale.ScaleY + Canvas.GetTop(mapImage);
-            Canvas.SetLeft(combo, ((-1197.492 * mapInfo.pixelsPerCoordinate + mapInfo.origo.X) * scale.ScaleX + Canvas.GetLeft(mapImage)) - combo.ActualWidth / 2);
-            Canvas.SetTop(combo, ((998.6943 * -mapInfo.pixelsPerCoordinate + mapInfo.origo.Y) * scale.ScaleY + Canvas.GetTop(mapImage)) - combo.ActualHeight / 2);
+            //rbTranslateTransform.X = x;// + 18 * scale.ScaleX;
+            //rbTranslateTransform.Y = y;// + 18 * scale.ScaleY;
+            scaleLabel.Content = "X:" + rbTranslateTransform.X.ToString() +
+                " Y:" + rbTranslateTransform.Y.ToString() + " S:" + scale.ScaleY.ToString();
 
             if (MainWindow.Instance.CurrentSave == null)
                 return;
@@ -239,6 +254,9 @@ namespace The_Long_Dark_Save_Editor_2.Tabs
         {
             var playerCanvasX = (playerPosition.X * mapInfo.pixelsPerCoordinate + mapInfo.origo.X) * scale.ScaleX + canvasXOffset;
             var playerCanvasY = (playerPosition.Y * -mapInfo.pixelsPerCoordinate + mapInfo.origo.Y) * scale.ScaleY + canvasYOffset;
+
+            playerLabel.Content = "X:" + (playerCanvasX - Canvas.GetLeft(rbButton) - 9).ToString() +
+                " Y:" + (playerCanvasY - Canvas.GetTop(rbButton) - 9).ToString() + " S:" + scale.ScaleX.ToString();
 
             Canvas.SetLeft(player, playerCanvasX);
             Canvas.SetTop(player, playerCanvasY);
